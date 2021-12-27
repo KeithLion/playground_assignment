a8456547c406d867da01f6da9ffbe275d2ffec19
KeithLion <nortonkiara6@gmail.com>
2021-12-26 21:47:19 -0800

    created the first route in the server

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect
+from survey import Survey
+app = Flask(__name__)
+app.secret_key = 'playplace'
+
+
+@app.route('/')
+def starter_boxes():
+    return render_template('boxes.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
